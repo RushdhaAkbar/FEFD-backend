9e2af37cb627e7522d90a22cc722046cec914a28
Rushdha <rushdhaakbar82@gmail.com>
2025-02-23 14:58:25 +0530

    typescript type error resolved

--- src/api/middleware/authorization-middleware.ts
+++ src/api/middleware/authorization-middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import ForbiddenError from "../../domain/errors/forbidden-error";
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-  const auth = req.auth;
+    const auth = (req as any).auth;
   if (auth?.sessionClaims?.metadata.role !== "admin") {
     throw new ForbiddenError("Forbidden");
   }
